cf5aa28dd1362b0fcbf4272c6858e85eafa551b2
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-09-09 11:08:13 +1000

    Update 02-task.sql

--- 0x14-mysql/02-task.sql
+++ 0x14-mysql/02-task.sql
@@ -1,5 +1,6 @@
 -- creates the database
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
 CREATE TABLE IF NOT EXISTS nexus6 (id INT, name VARCHAR(256));
 INSERT INTO nexus6 (`id`, `name`) VALUES (1, 'Leon');
-GRANT SELECT ON *.* TO 'holberton_user'@'localhost';
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
