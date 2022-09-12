91932fe46a87a59002f8d86de1da650fe7692562
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-12 19:20:32 +1000

    add \n

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,23 +11,23 @@ if __name__ == '__main__':
     employee_ID = int(argv[1])
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
         employee_ID)
-    employee = requests.get(user_url)
-    employee_name = employee.json().get('name')
-    user_id = employee.json().get('id')
+    employee_response = requests.get(user_url)
+    employee_name = employee_response.json().get('name')
+    user_id = employee_response.json().get('id')
     todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
         employee_ID)
-    todo = requests.get(todo_url).json()
+    todo_json = requests.get(todo_url).json()
     total_tasks = 0
     num_completed_tasks = 0
     completed_tasks = []
 
-    for task in todo:
+    for task in todo_json:
         total_tasks += 1
         if task.get('completed') is True:
             completed_tasks.append(task.get('title'))
             num_completed_tasks += 1
 
-    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
+    print("Employee {} is done with tasks({}/{}):\n".format(employee_name,
           num_completed_tasks, total_tasks))
     for item in completed_tasks:
         print("\t {}".format(item))
